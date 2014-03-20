904469bb94e7cb49cabc36521d51702f6736be91
jwondra <jacob.wondra@gmail.com>
2014-03-19 22:47:44 -0500

    initial project version

---
+++ src/main/java/com/guysafternoonin/betterbeds/BetterBeds.java
@@ -0,0 +1,19 @@
+package com.guysafternoonin.betterbeds;
+
+import net.minecraft.init.Blocks;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+
+@Mod(modid = BetterBeds.MODID, version = BetterBeds.VERSION)
+public class BetterBeds
+{
+    public static final String MODID = "Better Beds";
+    public static final String VERSION = "0.1";
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+
+    }
+}
